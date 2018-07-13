2bd684d0841fd7c5cd3e86827c219ca7da693335
Alex <the_smolniy@yahoo.com>
2018-07-13 16:39:32 +0300

    2nd

--- javascript/module-9/js/index.js
+++ javascript/module-9/js/index.js
@@ -11,6 +11,7 @@ let isActive = false;
 
 let startTime = Date.now();
 let deltaTime = 0;
+let stopTime;
 
 let minutes;
 let seconds;
@@ -20,6 +21,7 @@ reset.disabled = true;
 lap.disabled = true;
 start.addEventListener('click', handleStart);
 lap.addEventListener('click', handleLap);
+reset.addEventListener('click', handleReset);
 
 function handleStart(){
   if(!isActive){
@@ -28,8 +30,10 @@ function handleStart(){
     reset.disabled = false;
     lap.disabled = false;
     timerId = setInterval(updateValue, 100);
-    start.addEventListener('click', handlePause);
-    reset.addEventListener('click', handleReset);
+    
+  }
+  else {
+    handlePause();
   }
 }
 
@@ -47,47 +51,5 @@ function handlePause() {
   clearInterval(timerId);
   start.textContent = 'continue';
   isActive = true;
-  const stopTime = Date.now();
-  start.addEventListener('click', goingAfterPause);
-  
-  
-  function goingAfterPause() {
-    clearInterval(timerId);
-    timerId = setInterval(valueAfterPause, 100);
-    start.textContent = 'Pause';
-    
-    lap.addEventListener('click', handleLap);
-    start.addEventListener('click', handlePause);
-    
-  }
-  function valueAfterPause(){
-    const currentTime = Date.now(stopTime);
-    deltaTime = currentTime - startTime ;
-    
-    const timeNow = new Date(deltaTime);
-    minutes = timeNow.getMinutes();
-    seconds = timeNow.getSeconds();
-    milliseconds = Number.parseInt(timeNow.getMilliseconds() / 100 );
-    
-    time.textContent = `${minutes}:${seconds}.${milliseconds}`;
-  }
-}
-
-
-function handleLap(){
- const newLap = document.createElement('li');
- newLap.textContent = `${minutes}:${seconds}.${milliseconds}`;
- laps.appendChild(newLap);
-}
-function handleReset() {
-  isActive = false;
-  clearInterval(timerId);
-  time.textContent = '00:00.0';
-  start.textContent = 'Start';
-  reset.disabled = true;
-  deltaTime = 0;
-  startTime - null;
-  laps.remove(stroke);
-} 
-
-
+  stopTime = Date.now();
+};  
