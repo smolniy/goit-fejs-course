6ca04582866b50cad5381cd32c3e6fdc956ee896
Alex <the_smolniy@yahoo.com>
2018-07-14 14:25:06 +0300

    readyhw

--- javascript/module-9/js/index.js
+++ javascript/module-9/js/index.js
@@ -7,13 +7,15 @@ const reset = document.querySelector('.js-reset');
 let laps = document.querySelector('.js-laps');
 let stroke = document.querySelectorAll('li');
 let timerId = null;
-let newTimerId = null;
 let isActive = false;
 
-let startTime = Date.now();
-let deltaTime = 0;
+let lapArr = Array.from(laps);
+let lightArr; 
+
+let startTime = null;
 let stopTime = 0;
 let timeNow = null;
+let currentTime;
 
 let minutes;
 let seconds;
@@ -21,37 +23,65 @@ let milliseconds;
 
 reset.disabled = true;
 lap.disabled = true;
+
 start.addEventListener('click', handleStart);
-//lap.addEventListener('click', handleLap);
-//reset.addEventListener('click', handleReset);
+lap.addEventListener('click', handleLap);
+reset.addEventListener('click', handleReset);
 
 function handleStart(){
   if(!isActive){
     isActive = true;
+    startTime = Date.now() + stopTime;
     start.textContent = 'Pause';
     reset.disabled = false;
     lap.disabled = false;
     timerId = setInterval(updateValue, 100);
-    
-  }
-  else {
+} else {
     handlePause();
   }
 }
 
 function updateValue(){
-  let currentTime = Date.now();
-  deltaTime = currentTime - startTime;
-  timeNow = new Date(deltaTime);
+  currentTime = Date.now();
+  timeNow = new Date(currentTime - startTime);
   minutes = timeNow.getMinutes();
   seconds = timeNow.getSeconds();
   milliseconds = Number.parseInt(timeNow.getMilliseconds() / 100 );
+  if (minutes < 10) minutes = '0' + minutes;
+  if (seconds < 10) seconds = '0' + seconds;
+  if (milliseconds === '') milliseconds = 0;
   time.textContent = `${minutes}:${seconds}.${milliseconds}`;
   
 }
 
 function handlePause() {
+  isActive = false;
+  stopTime = startTime - Date.now();
   clearInterval(timerId);
   start.textContent = 'continue';
+  lap.addEventListener('click', handleLap);
+  
+}
+function handleLap(){
+  const newLap = document.createElement('li');
+  newLap.textContent = `${minutes}:${seconds}.${milliseconds}`;
+  laps.appendChild(newLap);
+  lapArr.push(newLap.textContent);
+  lightArr = lapArr;
+  
+  console.log(lapArr);
+}
+ 
+function handleReset() {
   isActive = false;
-}  
+  clearInterval(timerId);
+  time.textContent = '00:00.0';
+  start.textContent = 'Start';
+  reset.disabled = true;
+  lap.disabled = true;
+  stopTime = 0;
+  startTime = null;
+  laps.remove(stroke);
+  lapArr = [];
+  location.reload(true)
+}   
